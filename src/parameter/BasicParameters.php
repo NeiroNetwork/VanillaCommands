d49a72e9ad11868609a26a338efa6f9f4d91d577
SkyLake-git <yasuepp2test@gmail.com>
2022-08-17 01:52:35 +0900

    50%くらいのコマンドを更新

--- src/parameter/BasicParameters.php
+++ src/parameter/BasicParameters.php
@@ -5,13 +5,55 @@ declare(strict_types=1);
 namespace NeiroNetwork\VanillaCommands\parameter;
 
 use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+use pocketmine\network\mcpe\protocol\types\command\CommandEnum;
 use pocketmine\network\mcpe\protocol\types\command\CommandParameter;
 use pocketmine\utils\CloningRegistryTrait;
 
 class BasicParameters {
 	#use CloningRegistryTrait;
 
-	public static function TARGETS(string $paramName, int $flags = 0, bool $optional = false): CommandParameter {
+	public static function targets(string $paramName, int $flags = 0, bool $optional = false): CommandParameter {
 		return CommandParameter::standard($paramName, AvailableCommandsPacket::ARG_TYPE_TARGET, $flags, $optional);
+	}
+
+	public static function int(string $paramName, int $flags = 0, bool $optional = false): CommandParameter {
+		return CommandParameter::standard($paramName, AvailableCommandsPacket::ARG_TYPE_INT, $flags, $optional);
+	}
+
+	public static function float(string $paramName, int $flags = 0, bool $optional = false): CommandParameter {
+		return CommandParameter::standard($paramName, AvailableCommandsPacket::ARG_TYPE_FLOAT, $flags, $optional);
+	}
+
+	public static function position(string $paramName, int $flags = 0, bool $optional = false): CommandParameter {
+		return CommandParameter::standard($paramName, AvailableCommandsPacket::ARG_TYPE_POSITION, $flags, $optional);
+	}
+
+	public static function string(string $paramName, int $flags = 0, bool $optional = false): CommandParameter {
+		return CommandParameter::standard($paramName, AvailableCommandsPacket::ARG_TYPE_STRING, $flags, $optional);
+	}
+
+	public static function item(string $paramName, int $flags = 0, bool $optional = false): CommandParameter {
+		return CommandParameter::standard($paramName, AvailableCommandsPacket::ARG_TYPE_STRING, $flags, $optional);
+		# Item がない...!?
+	}
+
+	public static function message(string $paramName, int $flags = 0, bool $optional = false): CommandParameter {
+		return CommandParameter::standard($paramName, AvailableCommandsPacket::ARG_TYPE_MESSAGE, $flags, $optional);
+	}
+
+	public static function merge(CommandParameter $param, int $type): CommandParameter {
+		$param->paramType |= $type;
+		return $param;
+	}
+
+	public static function enum(string $name, string $enumName, ?array $enumValues = null, int $flags = 1, bool $optional = false) {
+		if ($enumValues === null) {
+			$enumValues = [$enumName];
+		}
+		return CommandParameter::enum($name, new CommandEnum($enumName, $enumValues), $flags, $optional);
+	}
+
+	public static function json(string $name, int $flags = 0, bool $optional = false): CommandParameter {
+		return CommandParameter::standard($name, AvailableCommandsPacket::ARG_TYPE_JSON, $flags, $optional);
 	}
 }
--- src/parameter/Parameter.php
+++ src/parameter/Parameter.php
@@ -28,7 +28,87 @@ class Parameter {
 		$this->list = [];
 
 		$this->add("kill", [
-			BasicParameters::TARGETS("target")
+			BasicParameters::targets("target", optional: true)
+		]);
+
+		$this->add("clear", [
+			BasicParameters::targets("player", optional: true),
+			BasicParameters::item("itemName", optional: true),
+			BasicParameters::int("data", optional: true),
+			BasicParameters::int("maxCount", optional: true)
+		]);
+
+		$this->add("kick", [
+			BasicParameters::targets("name"),
+			BasicParameters::message("reason", optional: true)
+		]);
+
+		$this->add("time", [
+			BasicParameters::enum("add", "add"),
+			BasicParameters::int("amount")
+		]);
+
+		$this->add("time", [
+			BasicParameters::enum("set", "set"),
+			BasicParameters::merge(BasicParameters::string("time"), AvailableCommandsPacket::ARG_TYPE_INT)
+		]);
+
+		$this->add("time", [
+			BasicParameters::enum("query", "query")
+		]);
+
+		$this->add("time", [
+			BasicParameters::enum("start", "start")
+		]);
+
+		$this->add("time", [
+			BasicParameters::enum("stop", "stop")
+		]);
+
+		$this->add("enchant", [
+			BasicParameters::targets("player"),
+			BasicParameters::int("enchantmentId"),
+			BasicParameters::int("level", optional: true)
+		]);
+
+		$this->add("enchant", [
+			BasicParameters::targets("player"),
+			BasicParameters::string("enchantmentName"),
+			BasicParameters::int("level", optional: true)
+		]);
+
+		$this->add("tp", [
+			BasicParameters::targets("victim"),
+			BasicParameters::targets("destination")
+		]);
+
+		$this->add("tp", [
+			BasicParameters::targets("victim"),
+			BasicParameters::position("destination"),
+			BasicParameters::float("yRot", optional: true),
+			BasicParameters::float("xRot", optional: true)
+		]);
+
+		$this->add("tp", [
+			BasicParameters::targets("destination")
+		]);
+
+		$this->add("tp", [
+			BasicParameters::position("destination"),
+			BasicParameters::float("yRot", optional: true),
+			BasicParameters::float("xRot", optional: true)
+		]);
+
+		$this->add("gamemode", [
+			BasicParameters::string("gamemode"),
+			BasicParameters::targets("player", optional: true)
+		]);
+
+		$this->add("give", [
+			BasicParameters::targets("player"),
+			BasicParameters::string("item : data(optional)"),
+			BasicParameters::int("amount", optional: true),
+			BasicParameters::json("nbt", optional: true)
 		]);
 	}
 
